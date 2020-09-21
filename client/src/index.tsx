0ec19e82db0f8562e875dc0a94b277c19ba8331e
Diogo Silvério <dblsilverio+github@gmail.com>
2020-09-21 17:40:40 -0300

    Frontend client draft

---
+++ client/src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          Main
+        </Route>
+        <Route exact path="/login">
+          Login
+        </Route>
+        <Route exact path="/callback">
+          Callback
+        </Route>
+        <Route exact path="/collections">
+          Collections
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
