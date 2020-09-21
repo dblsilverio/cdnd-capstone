ca0a132941c4053afc8d7d4fd421e9bc753d1f15
Diogo Silvério <dblsilverio+github@gmail.com>
2020-09-21 16:36:51 -0300

    Fixing image URL

--- image-reader/src/utils/infra/helpers.ts
+++ image-reader/src/utils/infra/helpers.ts
@@ -1,7 +1,7 @@
 const bucketName = process.env.IMAGES_BUCKET_NAME
 
 export function completeImageUrl(filename: string): string {
-    return `https://${bucketName}.s3.amazonws.com/${filename}`
+    return `https://${bucketName}.s3.amazonaws.com/${filename}`
 }
 
 export function timeMillis(): number {
